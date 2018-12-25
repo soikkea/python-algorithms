6d6333330c59d5755d2d8939970c970b2b242175
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2018-12-25 15:52:21 +0200

    Multivariate regression

--- machine_learning/regression.py
+++ machine_learning/regression.py
@@ -22,6 +22,27 @@ def mean_regression_predict(test, mean):
     return y_pred
 
 
+def multivariate_regression(train, test):
+    w_opt = multivariate_regression_fit(train)
+    y_pred = multivariate_regression_predict(test, w_opt)
+    return y_pred
+
+
+def multivariate_regression_fit(train):
+    X = train.X.values
+    XTX = X.transpose().dot(X)
+    y = train.y.values
+    XTR = X.transpose().dot(y)
+    w_opt = np.linalg.solve(XTX, XTR)
+    return w_opt
+
+
+def multivariate_regression_predict(test, w_opt):
+    X = test.X.values
+    y_pred = X.dot(w_opt)
+    return y_pred
+
+
 def MSE(y_pred, y_true):
     if y_pred.shape != y_true.shape:
         y_pred = y_pred.reshape((-1, 1))
