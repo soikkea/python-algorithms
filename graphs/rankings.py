ad29f795fd801335ff177ecc90c929a71c3ea6a6
Ari-Pekka Soikkeli <ari.soikkeli@gmail.com>
2019-01-03 17:35:44 +0200

    Polished graphs

--- graphs/rankings.py
+++ graphs/rankings.py
@@ -5,26 +5,58 @@ import pandas as pd
 
 
 def hubs_and_authorities(matrix, limit=25, disp_iterations=False):
+    """Hubs and authorities algorithm.
+    Also known as Hyperlink-Induced Topic Search.
+
+    Gives each node two scores:
+    Hub score is based on how many other nodes the node links to. It is
+    calculated as a sum of authority scores of nodes the node points to.
+    Authority score is based on how many other nodes link to the node. It is
+    calculated as a sum of hub scores of nodes pointing to the node.
+
+    After each iteration the scores are normalized.
+
+    Full description at:
+    https://en.wikipedia.org/wiki/HITS_algorithm
+
+    Arguments:
+        matrix {DataFrame} -- Pandas dataframe containing square adjacency matrix.
+
+    Keyword Arguments:
+        limit {int} -- Number of iterations. (default: {25})
+        disp_iterations {bool} -- Print status every iteration. (default: {False})
+
+    Returns:
+        DataFrame -- Results.
+    """
+
     matrix_values = matrix.values
     rows, columns = matrix_values.shape
 
-    # TODO: Error handling
-    assert rows == columns
-    # Initialize values
-    hubs = np.ones((rows, 1)) * (1.0 / np.sqrt(rows))
-    authorities = np.ones((rows, 1)) * (1.0 / np.sqrt(rows))
+    assert rows == columns, ("Number of rows and columns in matrix does not "
+                             "match")
 
+    # Initialize values
+    init_value = 1.0 / np.sqrt(rows)
+    hubs = np.ones((rows, 1)) * (init_value)
+    authorities = np.ones((rows, 1)) * (init_value)
+
+    # Main iteration loop
     for counter in range(limit):
 
         h_old = hubs.copy()
         a_old = authorities.copy()
 
-        for s in range(rows):
-            a_mask = matrix_values[s, :] > 0
-            h_mask = matrix_values[:, s] > 0
-            hubs[s] = authorities[a_mask].sum()
-            authorities[s] = hubs[h_mask].sum()
-        
+        for node in range(rows):
+            # Nodes the current node points to
+            a_mask = matrix_values[node, :] > 0
+            # Nodes pointing to the current node
+            h_mask = matrix_values[:, node] > 0
+
+            hubs[node] = authorities[a_mask].sum()
+            authorities[node] = hubs[h_mask].sum()
+
+        # Normalize scores
         hubs = hubs / np.linalg.norm(hubs)
         authorities = authorities / np.linalg.norm(authorities)
 
@@ -34,16 +66,47 @@ def hubs_and_authorities(matrix, limit=25, disp_iterations=False):
                 np.sqrt(np.sum((authorities - a_old) ** 2)),
                 np.sqrt(np.sum((hubs - h_old) ** 2))
             ))
-    
+
     df_out = pd.DataFrame(index=matrix.index)
     df_out['Hubs'] = hubs
     df_out['Authorities'] = authorities
-    df_out['Initial'] = 1.0 / np.sqrt(rows)
+    df_out['Initial'] = init_value
 
     return df_out
 
 
 def page_rank(matrix, epsilon=0.001, limit=50):
+    """PageRank algorithm.
+
+    Ranks nodes in a manner that gives high score to nodes which are linked to
+    by other nodes with high scores.
+    Final ranks are equivalent to probability distribution of random traveller
+    arraiving to specific node.
+
+    The algorithm is implemented as follows:
+    First, the matrix F is calculated. Its elements are defined as:
+    F_st = (1 - eps) / deg(t) + eps / N, if t links to s,
+         = 1 / N                       , if t doesn't link anywhere,
+         = eps / N                     , otherwise.
+    Here, deg(t) is the number of links from t, N is the total number of nodes
+    and eps is some small constant.
+    Second, the final ranks r_n are computed iteratively r_i = F * r_i-1 until
+    iteration limit i=n is reached.
+
+    Full description at:
+    https://en.wikipedia.org/wiki/PageRank
+
+    Arguments:
+        matrix {DataFrame} -- Pandas dataframe containing square adjacency matrix.
+
+    Keyword Arguments:
+        epsilon {float} -- Constant value. (default: {0.001})
+        limit {int} -- Number of iterations. (default: {50})
+
+    Returns:
+        DataFrame -- Results.
+    """
+
     matrix_values = matrix.values
     rows, columns = matrix_values.shape
 
@@ -51,13 +114,13 @@ def page_rank(matrix, epsilon=0.001, limit=50):
 
     for s in range(rows):
         for t in range(columns):
-            t_row_sum = matrix_values[t, :].sum()
+            t_n_links = matrix_values[t, :].sum()
             # If t is connected to s:
             if matrix_values[t, s] == 1:
                 ranks[s, t] = epsilon / float(rows)
-                if not np.isclose(t_row_sum, 0):
-                    ranks[s, t] = (1.0 - epsilon) / t_row_sum
-            elif np.isclose(t_row_sum, 0):
+                assert not np.isclose(t_n_links, 0)
+                ranks[s, t] += (1.0 - epsilon) / t_n_links
+            elif np.isclose(t_n_links, 0):
                 ranks[s, t] = 1.0 / float(rows)
             else:
                 ranks[s, t] = epsilon / float(rows)
@@ -74,7 +137,7 @@ def page_rank(matrix, epsilon=0.001, limit=50):
             if i == limit - 1:
                 final_ranks[x] = relevance[x]
         old_relevance = relevance.copy()
-    
+
     df_out = pd.DataFrame(final_ranks, index=matrix.index, columns=['Rank'])
 
     return df_out
